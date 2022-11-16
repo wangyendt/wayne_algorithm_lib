3537e2c7695f16ab6f66515b4937f3e23766bd8d
Ye Wang <905317742@qq.com>
2022-11-16 15:04:37 +0800

    update to 1.0.0.3.4
    增加1欧元滤波器

--- pywayne/dsp.py
+++ pywayne/dsp.py
@@ -320,3 +320,37 @@ class CurveSimilarity:
             return np.squeeze([
                 np.array(r)[np.argpartition(r, -k)[-k:]].mean() for r in ret
             ])
+
+
+class OneEuroFilter:
+    """
+    常用于pose tracking等场景
+    低速时去抖，高速时紧跟
+    https://gery.casiez.net/1euro/
+    https://gery.casiez.net/1euro/InteractiveDemo/
+    """
+
+    def __init__(self, te, mincutoff=1.0, beta=0.007, dcutoff=1.0):
+        self.x = None
+        self.dx = 0
+        self.te = te
+        self.mincutoff = mincutoff
+        self.beta = beta
+        self.dcutoff = dcutoff
+        self.alpha = self._alpha(self.mincutoff)
+        self.dalpha = self._alpha(self.dcutoff)
+
+    def _alpha(self, cutoff):
+        tau = 1.0 / (2 * np.pi * cutoff)
+        return 1.0 / (1.0 + tau / self.te)
+
+    def predict(self, x, te):
+        result = x
+        if self.x is not None:
+            edx = (x - self.x) / te
+            self.dx = self.dx + (self.dalpha * (edx - self.dx))
+            cutoff = self.mincutoff + self.beta * abs(self.dx)
+            self.alpha = self._alpha(cutoff)
+            result = self.x + self.alpha * (x - self.x)
+        self.x = result
+        return result
--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ install_reqs = parse_requirements('requirements.txt')
 
 setuptools.setup(
     name="pywayne",
-    version="1.0.0.3.3",
+    version="1.0.0.3.4",
     author="Wayne",
     author_email="wang121ye@hotmail.com",
     description="Some useful tools",
