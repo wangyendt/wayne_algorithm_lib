bd9a835ad6ff73c4bc454e51c75e76a20cbe0fc5
ye.wang <905317742@qq.com>
2023-11-06 20:33:26 +0800

    update to 1.0.0.5.3
    add clean download option

--- bin/gettool.py
+++ bin/gettool.py
@@ -16,7 +16,7 @@ import os
 import yaml
 
 
-def fetch_tool(tool_name, target_dir='', build=False):
+def fetch_tool(tool_name, target_dir='', build=False, clean=False):
     print(f"Fetching tool: {tool_name}")
     cwd = os.getcwd()
     with tempfile.TemporaryDirectory() as temp_dir:
@@ -45,9 +45,28 @@ def fetch_tool(tool_name, target_dir='', build=False):
         else:
             if os.path.exists(target_dir):
                 shutil.rmtree(target_dir)
-            shutil.copytree(tool_path, target_dir)
+            if clean:
+                shutil.copytree(os.path.join(tool_path, 'src'), target_dir)
+            else:
+                shutil.copytree(tool_path, target_dir)
         print(f"Tool {tool_name} has been copied to {target_dir}")
     os.chdir(cwd)
+
+
+def print_supported_tools():
+    with tempfile.TemporaryDirectory() as temp_dir:
+        os.chdir(temp_dir)
+        subprocess.run(
+            ["git", "clone", "--sparse", "https://github.com/wangyendt/cpp_tools", temp_dir],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL
+        )
+        name_to_path_map_yaml_file = 'name_to_path_map.yaml'
+        assert name_to_path_map_yaml_file in os.listdir('.')
+        with open(name_to_path_map_yaml_file, 'r') as f:
+            name_to_path_map = yaml.safe_load(f)
+            print('Currently supported tools:')
+            print(', '.join(name_to_path_map.keys()))
 
 
 def main():
@@ -59,19 +78,26 @@ def main():
     parser.add_argument('-b', '--build', action='store_true', help='Whether to build lib')
     parser.add_argument('-U', '--upgrade', action='store_true', help='Upgrade the tool')
     parser.add_argument('-f', '--force', action='store_true', help='Force action')
+    parser.add_argument('-c', '--clean', action='store_true', help='Only fetch c++ sources')
+    parser.add_argument('-l', '--list', action='store_true', help='List current supported tools')
 
     args = parser.parse_args()
+
+    if args.list:
+        print_supported_tools()
+        return
 
     # 如果通过 -n 或 --name 提供了名称，则使用它，否则使用位置参数提供的名称
     tool_name = args.name if args.name is not None else args.name_pos
     target_path = args.target_path
     build = args.build
+    clean = args.clean
 
     # 检查是否提供了名称
     if tool_name is None:
         parser.error("the following arguments are required: name")
 
-    fetch_tool(tool_name, target_dir=target_path, build=build)
+    fetch_tool(tool_name, target_dir=target_path, build=build, clean=clean)
 
     if args.upgrade:
         print("(not implemented yet)")
